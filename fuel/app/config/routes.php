7a0849f7abda19f3fa9344b7de1f713431288f70
Tomohiro Minami <globalue.minami@gmail.com>
2015-05-30 17:25:27 +0900

    route 更

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-	'_root_'  => 'web/landing', // Root (Web Module)
+	'_root_'  => 'welcome', // Root (Web Module)
 	'_404_'   => '404',    // The main 404 route
 
 	/**
@@ -9,14 +9,6 @@ return array(
 	 * モジュールを追加したらここも追加する
 	 *
 	 */
-	'web'  => 'web/landing',
-	'web/(:any)'  => 'web/$1',
 	'api/(:any)'  => 'api/$1',
-	'admin/(:any)'  => 'admin/$1',
-	'sandbox/(:any)'  => 'sandbox/$1',
 	'table/(:any)' => 'table/$1',
-
-	// モジュール名を含まない場合はwebモジュールへ
-	'(:any)'  => 'web/$1',
-
 );
